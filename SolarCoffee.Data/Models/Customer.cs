26aa8136ed6cc6396aaaf5a9a165f298c923186c
Zokir Sobirjonov <zokir5857@gmail.com>
2020-08-28 14:39:56 +0500

    Add models and create EF Core migration

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+   public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}

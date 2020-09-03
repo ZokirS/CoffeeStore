68f9e66ea38c37ca2d803f6403136ded53d294c7
Zokir Sobirjonov <zokir5857@gmail.com>
2020-09-03 14:05:42 +0500

    Create view models, update InventoryController

---
+++ SolarCoffee.Web/Controllers/InventoryController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Services.Inventory;
+using SolarCoffee.Web.Serialization;
+using SolarCoffee.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Web.Controllers
+{
+    [ApiController]
+    public class InventoryController: ControllerBase
+    {
+        private readonly IInventoryService _inventory;
+        private readonly ILogger<InventoryController> _logger;
+
+        public InventoryController(IInventoryService inventory, ILogger<InventoryController> logger)
+        {
+            _inventory = inventory;
+            _logger = logger;
+        }
+
+        [HttpGet("/api/inventory")]
+        public ActionResult GetCurrentInventory()
+        {
+            _logger.LogInformation("Getting all inventory");
+            var inventory = _inventory.GetCurrentInventory()
+                .Select(pi => new ProductInventoryModel
+                {
+                    Id=pi.Id,
+                    Product=ProductMapper.SerializeProductModel(pi.Product),
+                    IdealQuantity=pi.IdealQuantity,
+                    QuantiryOnHand=pi.QuantityOnHand
+                })
+              .OrderBy(inv => inv.Product.Name)
+              .ToList();
+
+            return Ok(inventory);
+        }
+
+        [HttpPatch("/api/inventory")]
+        public ActionResult UpdateInventory([FromBody] ShipmentModel shipment)
+        {
+            _logger.LogInformation($"Updating inventory for {shipment.ProductId} - Adjustment: {shipment.Adjustment}");
+
+            var id = shipment.ProductId;
+            var adjustment = shipment.Adjustment;
+            var inventory = _inventory.UpdateUnitsAvailable(id, adjustment);
+            return Ok(inventory);
+        }
+    }
+}
--- SolarCoffee.Web/Controllers/OrderController.cs
+++ SolarCoffee.Web/Controllers/OrderController.cs
@@ -33,5 +33,21 @@ namespace SolarCoffee.Web.Controllers
             _orderService.GenerateOpenOrder(order);
             return Ok();
         }
+
+        [HttpGet("/api/order")]
+        public ActionResult GetOrders()
+        {
+            var orders = _orderService.GetOrders();
+            var orderModels = OrderMapper.SerializeOrdersToViewModels(orders);
+            return Ok(orderModels);
+        }
+
+        [HttpPatch("/api/order/complete/{id}")]
+        public ActionResult GetOrders(int id)
+        {
+            _logger.LogInformation($"Marking order {id} complere ...");
+            _orderService.MarkFulfilled(id);
+            return Ok();
+        }
     }
 }
